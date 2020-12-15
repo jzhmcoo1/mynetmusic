8e9c33bb100ef1176b9dafdc4fce349276716328
jzhmcoo1 <jzhmzlxcoo1@gmail.com>
2020-12-15 15:16:46 +0800

    添加了readme文件

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,5 @@
 // 配置服务器相关信息
 export default {
     // host: 'http://localhost:3000', //本地测试接口
-    // mobileHost: 'https://lixingzhu.cn.utools.club', //内网穿透测试接口
     ipHost: 'https://www.lihangzhu.com:8443' // 公网ip服务器测试接口
 }
